4aa0caf88aa3a819674cb571669ad9904d497918
Hyeen <81229303+hyeen24@users.noreply.github.com>
2025-10-14 20:48:26 +0800

    Updated wrapper background color

--- components/ScreenWrapper.tsx
+++ components/ScreenWrapper.tsx
@@ -2,16 +2,19 @@ import { View, Text, Dimensions, Platform, StatusBar } from 'react-native'
 import React from 'react'
 import { ScreenWrapperProps } from '@/types'
 import Colors from '@/constants/Colors';
+import { useTheme } from '@/contexts/ThemeContext';
 
 const {height} = Dimensions.get('window');
 
 const ScreenWrapper = ({style, children}: ScreenWrapperProps) => {
     let paddingTop = Platform.OS == 'ios' ? height * 0.06 : 50;
+
+    const { theme } = useTheme();
   return (
     <View style={[{
         paddingTop,
         flex: 1,
-        backgroundColor: Colors.black
+        backgroundColor: theme.backgroundColor
         }, 
         style,
         ]}>
